67a61abd51fb58e958d7367097e63878a45a33b1
Goeran Poehner <goeran.poehner@gmail.com>
2023-01-22 15:34:03 +0100

    Adapted demo for multiple publisher threads

--- src/main/java/de/goeri/App.java
+++ src/main/java/de/goeri/App.java
@@ -5,6 +5,8 @@ import org.apache.camel.Endpoint;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.impl.DefaultCamelContext;
 
+import java.util.ArrayList;
+
 public class App 
 {
     public static void main( String[] args ) throws Exception
@@ -13,32 +15,39 @@ public class App
         context.start();
         // Create an exchange and a queue with overflow behavior "reject-publish" and max. length of 10 entries
         // Also use publish acknowledgements for this demo
-        Endpoint endpoint = context.getEndpoint("rabbitmq://127.0.0.1:5672/test_exchg?username=guest&password=guest&autoDelete=false&routingKey=#&queue=test_queue&channelPoolMaxSize=30&channelPoolMaxWait=1000&publisherAcknowledgements=true&publisherAcknowledgementsTimeout=2000&arg.queue.x-overflow=reject-publish&arg.queue.x-max-length=10");
+        Endpoint endpoint = context.getEndpoint("rabbitmq://127.0.0.1:5672/test_exchg?username=guest&password=guest&autoDelete=false&routingKey=#&queue=test_queue&channelPoolMaxSize=100&channelPoolMaxWait=1000&publisherAcknowledgements=true&publisherAcknowledgementsTimeout=2000&arg.queue.x-overflow=reject-publish&arg.queue.x-max-length=10");
         final ProducerTemplate p = context.createProducerTemplate();
         p.setDefaultEndpoint(endpoint);
         // Publish a lot of messages in an endless loop to the exchange/queue
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                boolean loop=true;
-                while (loop && !Thread.currentThread().isInterrupted()) {
-                    try {
+
+        ArrayList<Thread> workers = new ArrayList<>();
+        for (int i=0; i<5; i++) {
+            Thread t = new Thread(new Runnable() {
+                public void run() {
+                    boolean loop = true;
+                    while (loop && !Thread.currentThread().isInterrupted()) {
                         try {
-                            p.sendBody("Hello");
-                        } catch (CamelExecutionException e) {
-                            System.out.println(e.getCause());
+                            try {
+                                p.sendBody("Hello");
+                            } catch (CamelExecutionException e) {
+                                System.out.println(e.getCause());
+                            }
+                            Thread.sleep(5);
+                        } catch (InterruptedException e) {
+                            loop = false;
                         }
-                        Thread.sleep(1);
-                    } catch (InterruptedException e) {
-                        loop = false;
                     }
                 }
-            }
-        });
-        t.start();
+            });
+            workers.add(t);
+        }
+        workers.forEach(w -> w.start());
 
         // Send messages in background for 1 minute and then quit
         Thread.sleep(60000);
-        t.interrupt();
+        workers.forEach(w -> w.interrupt());
+
+        Thread.sleep(5000);
 
         context.stop();
     }
